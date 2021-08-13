fc84333628b9c40a346e6eac62e4595db675f3b8
Kirill Romanov <djaler1@gmail.com>
2021-08-13 20:03:17 +0300

    chore: remove pre-validation for `pnpm-wrapper use`

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { delegateToPnpm } from '@/delegate';
 import { useVersion } from '@/use-version';
-import { isVersionValid } from '@/version-manager';
 
 const args = process.argv.slice(2);
 
 switch (args[0]) {
     case 'use': {
         const requiredVersion = args[1];
-        if (!isVersionValid(requiredVersion)) {
-            throw new Error(`"${requiredVersion}" is not a valid version`);
-        }
         useVersion(requiredVersion);
         break;
     }
