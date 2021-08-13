1a2c3f42882cdc6e21b3470f45146148ffce018a
Kirill Romanov <djaler1@gmail.com>
2021-08-13 20:00:22 +0300

    fix: write version to pnpm.version only after success `pnpm-wrapper use`

--- src/use-version.ts
+++ src/use-version.ts
@@ -2,7 +2,7 @@ import { installPnpmIfNotInstalled } from '@/pnpm-manager';
 import { writeVersion } from '@/version-manager';
 
 export function useVersion(version: string) {
-    writeVersion(version);
-
     installPnpmIfNotInstalled(version);
+
+    writeVersion(version);
 }
